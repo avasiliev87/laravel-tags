53de24e5b5955330c512248c815568ea20457f9a
alex <alexander.vasiliev87@yandex.com>
2018-09-21 02:17:50 +1000

    mariadb support

--- src/Tag.php
+++ src/Tag.php
@@ -2,94 +2,256 @@
 
 namespace Spatie\Tags;
 
-use Spatie\EloquentSortable\Sortable;
+use InvalidArgumentException;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Translatable\HasTranslations;
 use Illuminate\Database\Eloquent\Builder;
-use Spatie\EloquentSortable\SortableTrait;
-use Illuminate\Database\Eloquent\Collection as DbCollection;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
-class Tag extends Model implements Sortable
+trait HasTags
 {
-    use SortableTrait, HasTranslations, HasSlug;
+    protected $queuedTags = [];
 
-    public $translatable = ['name', 'slug'];
-
-    public $guarded = [];
-
-    public function scopeWithType(Builder $query, string $type = null): Builder
+    public static function getTagClassName(): string
     {
-        if (is_null($type)) {
-            return $query;
-        }
-
-        return $query->where('type', $type)->orderBy('order_column');
+        return Tag::class;
     }
 
-    public function scopeContaining(Builder $query, string $name, $locale = null): Builder
+    public static function bootHasTags()
     {
-        $locale = $locale ?? app()->getLocale();
+        static::created(function (Model $taggableModel) {
+            $taggableModel->attachTags($taggableModel->queuedTags);
 
-        return $query->whereRaw('LOWER(JSON_EXTRACT(name, "$.'.$locale.'")) like ?', ['"%'.strtolower($name).'%"']);
+            $taggableModel->queuedTags = [];
+        });
+
+        static::deleted(function (Model $deletedModel) {
+            $tags = $deletedModel->tags()->get();
+
+            $deletedModel->detachTags($tags);
+        });
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this
+            ->morphToMany(self::getTagClassName(), 'taggable')
+            ->orderBy('order_column');
     }
 
     /**
-     * @param array|\ArrayAccess $values
-     * @param string|null $type
-     * @param string|null $locale
-     *
-     * @return \Spatie\Tags\Tag|static
+     * @param string|array|\ArrayAccess|\Spatie\Tags\Tag $tags
      */
-    public static function findOrCreate($values, string $type = null, string $locale = null)
+    public function setTagsAttribute($tags)
     {
-        $tags = collect($values)->map(function ($value) use ($type, $locale) {
+        if (! $this->exists) {
+            $this->queuedTags = $tags;
+
+            return;
+        }
+
+        $this->attachTags($tags);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array|\ArrayAccess|\Spatie\Tags\Tag $tags
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithAllTags(Builder $query, $tags, string $type = null): Builder
+    {
+        $tags = static::convertToTags($tags, $type);
+
+        collect($tags)->each(function ($tag) use ($query) {
+            $query->whereIn("{$this->table}.{$this->getKeyName()}", function ($query) use ($tag) {
+                $query->from('taggables')
+                    ->select('taggables.taggable_id')
+                    ->where('taggables.tag_id', $tag ? $tag->id : 0);
+            });
+        });
+
+        return $query;
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array|\ArrayAccess|\Spatie\Tags\Tag $tags
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithAnyTags(Builder $query, $tags, string $type = null): Builder
+    {
+        $tags = static::convertToTags($tags, $type);
+
+        return $query->whereHas('tags', function (Builder $query) use ($tags) {
+            $tagIds = collect($tags)->pluck('id');
+
+            $query->whereIn('id', $tagIds);
+        });
+    }
+
+    public function tagsWithType(string $type = null): Collection
+    {
+        return $this->tags->filter(function (Tag $tag) use ($type) {
+            return $tag->type === $type;
+        });
+    }
+
+    /**
+     * @param array|\ArrayAccess|\Spatie\Tags\Tag $tags
+     *
+     * @return $this
+     */
+    public function attachTags($tags)
+    {
+        $className = static::getTagClassName();
+
+        $tags = collect($className::findOrCreate($tags));
+
+        $this->tags()->syncWithoutDetaching($tags->pluck('id')->toArray());
+
+        return $this;
+    }
+
+    /**
+     * @param string|\Spatie\Tags\Tag $tag
+     *
+     * @return $this
+     */
+    public function attachTag($tag)
+    {
+        return $this->attachTags([$tag]);
+    }
+
+    /**
+     * @param array|\ArrayAccess $tags
+     *
+     * @return $this
+     */
+    public function detachTags($tags)
+    {
+        $tags = static::convertToTags($tags);
+
+        collect($tags)
+            ->filter()
+            ->each(function (Tag $tag) {
+                $this->tags()->detach($tag);
+            });
+
+        return $this;
+    }
+
+    /**
+     * @param string|\Spatie\Tags\Tag $tag
+     *
+     * @return $this
+     */
+    public function detachTag($tag)
+    {
+        return $this->detachTags([$tag]);
+    }
+
+    /**
+     * @param array|\ArrayAccess $tags
+     *
+     * @return $this
+     */
+    public function syncTags($tags)
+    {
+        $className = static::getTagClassName();
+
+        $tags = collect($className::findOrCreate($tags));
+
+        $this->tags()->sync($tags->pluck('id')->toArray());
+
+        return $this;
+    }
+
+    /**
+     * @param array|\ArrayAccess $tags
+     * @param string|null $type
+     *
+     * @return $this
+     */
+    public function syncTagsWithType($tags, string $type = null)
+    {
+        $className = static::getTagClassName();
+
+        $tags = collect($className::findOrCreate($tags, $type));
+
+        $this->syncTagIds($tags->pluck('id')->toArray(), $type);
+
+        return $this;
+    }
+
+    protected static function convertToTags($values, $type = null, $locale = null)
+    {
+        return collect($values)->map(function ($value) use ($type, $locale) {
             if ($value instanceof Tag) {
+                if (isset($type) && $value->type != $type) {
+                    throw new InvalidArgumentException("Type was set to {$type} but tag is of type {$value->type}");
+                }
+
                 return $value;
             }
 
-            return static::findOrCreateFromString($value, $type, $locale);
+            $className = static::getTagClassName();
+
+            return $className::findFromString($value, $type, $locale);
         });
-
-        return is_string($values) ? $tags->first() : $tags;
     }
 
-    public static function getWithType(string $type): DbCollection
+    /**
+     * Use in place of eloquent's sync() method so that the tag type may be optionally specified.
+     *
+     * @param $ids
+     * @param string|null $type
+     * @param bool $detaching
+     */
+    protected function syncTagIds($ids, string $type = null, $detaching = true)
     {
-        return static::withType($type)->orderBy('order_column')->get();
-    }
+        $isUpdated = false;
 
-    public static function findFromString(string $name, string $type = null, string $locale = null)
-    {
-        $locale = $locale ?? app()->getLocale();
+        // Get a list of tag_ids for all current tags
+        $current = $this->tags()
+            ->newPivotStatement()
+            ->where('taggable_id', $this->getKey())
+            ->when($type !== null, function ($query) use ($type) {
+                $tagModel = $this->tags()->getRelated();
 
-        return static::query()
-            ->where("name->{$locale}", $name)
-            ->where('type', $type)
-            ->first();
-    }
+                return $query->join(
+                    $tagModel->getTable(),
+                    'taggables.tag_id',
+                    '=',
+                    $tagModel->getTable().'.'.$tagModel->getKeyName()
+                )
+                    ->where('tags.type', $type);
+            })
+            ->pluck('tag_id')
+            ->all();
 
-    protected static function findOrCreateFromString(string $name, string $type = null, string $locale = null): self
-    {
-        $locale = $locale ?? app()->getLocale();
-
-        $tag = static::findFromString($name, $type, $locale);
-
-        if (! $tag) {
-            $tag = static::create([
-                'name' => [$locale => $name],
-                'type' => $type,
-            ]);
+        // Compare to the list of ids given to find the tags to remove
+        $detach = array_diff($current, $ids);
+        if ($detaching && count($detach) > 0) {
+            $this->tags()->detach($detach);
+            $isUpdated = true;
         }
 
-        return $tag;
-    }
-
-    public function setAttribute($key, $value)
-    {
-        if ($key === 'name' && ! is_array($value)) {
-            return $this->setTranslation($key, app()->getLocale(), $value);
+        // Attach any new ids
+        $attach = array_diff($ids, $current);
+        if (count($attach) > 0) {
+            collect($attach)->each(function ($id) {
+                $this->tags()->attach($id, []);
+            });
+            $isUpdated = true;
         }
 
-        return parent::setAttribute($key, $value);
+        // Once we have finished attaching or detaching the records, we will see if we
+        // have done any attaching or detaching, and if we have we will touch these
+        // relationships if they are configured to touch on any database updates.
+        if ($isUpdated) {
+            $this->tags()->touchIfTouching();
+        }
     }
 }
